089cf5456723b8ba6f85f56c61c8504a5910c4f7
Khiara Danielle Rubia <khiara.rubia@usjr.edu.ph>
2021-09-07 22:50:55 +0800

    Apps Dev Projects

---
+++ 2_JS/LoopsTriangle.js
@@ -0,0 +1,8 @@
+var h = 6; //height
+var triangle = "*";
+
+for (var i = 1; i <= h; i++) {
+    for (var c = 1; c <= i; c++) {
+        console.log(triangle);
+    }
+}
---
+++ 2_JS/VariableType.js
@@ -0,0 +1,19 @@
+var array = [1, 2, 3, 4, 5, 6];
+var a;
+var object = { potato: " is yummy" };
+var b = null;
+var c = 100;
+var string = "MEHEHEHEHEHE"
+
+console.log("[" + array + "]");
+console.log("The variable " + array + " is a type of " + typeof array);
+console.log(object);
+console.log("The variable " + object + " is a type of " + typeof object);
+console.log(a);
+console.log("The variable " + a + " is a type of " + typeof a);
+console.log(b);
+console.log("The variable " + b + " is a type of " + typeof b);
+console.log(c);
+console.log("The variable " + c + " is a type of " + typeof c);
+console.log(string);
+console.log("The variable " + string + " is a type of " + typeof string);
